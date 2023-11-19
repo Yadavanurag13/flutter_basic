4a7f382fb797447e7c79256405d37e6cb30edf29
Anurag Yadav <yadavanurag1310@gmail.com>
2023-11-20 02:19:18 +0530

    third commit

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -13,55 +13,66 @@ class HomeView extends StatelessWidget {
         ),
         backgroundColor: Colors.amber,
       ),
-      body: const Center(
-        child: IconButton(
-          onPressed: null,
-          icon: Icon(Icons.alternate_email),
-          color: Colors.amber,
-        ),
-        //   child: ElevatedButton.icon(
-        //   onPressed: null,
-        //   icon: const Icon(Icons.mail),
-        //   label: const Text(
-        //     'Mail me',
-        //   ),
-        // )
-        // child: ElevatedButton(
-        //   onPressed: null,
-        //   style: ButtonStyle(
-        //     backgroundColor: MaterialStatePropertyAll(Colors.blueAccent),
-        //   ),
-        //   child: Text(
-        //     'Hello Button',
-        //     style: TextStyle(color: Colors.white),
-        //   ),
-        // ),
-
-        // child: ElevatedButton(
-        //   onPressed: null,
-        //   child: Text('Hello Anurag', style: TextStyle(fontSize: 50.0),),
-        // ),
-        //   child: Icon(
-        //   Icons.airport_shuttle,
-        //   color: Colors.lightBlue,
-        //   size: 70.0,
-        // )
-        // child: Image(
-        //   image: AssetImage(
-        //     'assets/anurag.jpeg',
-        //   ),
-        // ),
-        // child: Text(
-        //   'Hello anurag!',
-        //   style: TextStyle(
-        //     color: Colors.grey,
-        //     fontSize: 60.0,
-        //     fontWeight: FontWeight.bold,
-        //     letterSpacing: 2.0,
-        //     fontFamily: 'IndieFlower',
-        //   ),
-        // ),
+      body: const Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Text('Hello World'),
+        //we can not apply color property in paddling class
       ),
+      //   Container(
+      //   padding: const EdgeInsets.fromLTRB(30.0, 15.0 , 30.0, 15.0),
+      //   //EdgeInsets.all(20.0),
+      //   margin: const EdgeInsets.all(52.0),
+      //   color: Colors.grey,
+      //   child: const Text('Hello World'),
+      // ),
+      // const Center(
+      // child: IconButton(
+      //   onPressed: null,
+      //   icon: Icon(Icons.alternate_email),
+      //   color: Colors.amber,
+      // ),
+      //   child: ElevatedButton.icon(
+      //   onPressed: null,
+      //   icon: const Icon(Icons.mail),
+      //   label: const Text(
+      //     'Mail me',
+      //   ),
+      // )
+      // child: ElevatedButton(
+      //   onPressed: null,
+      //   style: ButtonStyle(
+      //     backgroundColor: MaterialStatePropertyAll(Colors.blueAccent),
+      //   ),
+      //   child: Text(
+      //     'Hello Button',
+      //     style: TextStyle(color: Colors.white),
+      //   ),
+      // ),
+
+      // child: ElevatedButton(
+      //   onPressed: null,
+      //   child: Text('Hello Anurag', style: TextStyle(fontSize: 50.0),),
+      // ),
+      //   child: Icon(
+      //   Icons.airport_shuttle,
+      //   color: Colors.lightBlue,
+      //   size: 70.0,
+      // )
+      // child: Image(
+      //   image: AssetImage(
+      //     'assets/anurag.jpeg',
+      //   ),
+      // ),
+      // child: Text(
+      //   'Hello anurag!',
+      //   style: TextStyle(
+      //     color: Colors.grey,
+      //     fontSize: 60.0,
+      //     fontWeight: FontWeight.bold,
+      //     letterSpacing: 2.0,
+      //     fontFamily: 'IndieFlower',
+      //   ),
+      // ),
       floatingActionButton: const FloatingActionButton(
         onPressed: null,
         backgroundColor: Colors.amber,
